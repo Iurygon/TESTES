d678e006e30a27c6c0712f73cd3a79890653297c
Iury Gontijo Silva <iurygon.silva@gmail.com>
2023-11-18 03:05:28 -0300

    Add files via upload

--- JavaScript/Sort/selectionSort.js
+++ JavaScript/Sort/selectionSort.js
@@ -1,41 +1,3 @@
-
-// const changeElements = require("./changeElements");
-
-// function changeElements(array, index){
-
-//     let actualElement = array[index];
-//     let previousElement = array[index - 1];
-
-//     array[index] = previousElement;
-//     array[index - 1] = actualElement;
-
-// };
-
-// function selectionSort(array){
-
-//     for (let i = 0; i < array.length - 1; i++){
-//         let minIndex = i;
-//         for (let j = i + 1; j < array.length; j++){
-//             if (array[j] < array[minIndex]){
-//                 minIndex = j;
-//             };
-//         };
-//         changeElements(array, minIndex);
-//     };
-//     return array;
-
-// };
-
-// console.log(selectionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]))
-
-function changeElements(array, index){
-    let actualElement = array[index];
-    let previousElement = array[index - 1];
-
-    array[index] = previousElement;
-    array[index - 1] = actualElement;
-}
-
 function selectionSort(array){
     for (let i = 0; i < array.length - 1; i++){
         let minIndex = i;
@@ -44,9 +6,13 @@ function selectionSort(array){
                 minIndex = j; // Update minIndex to the index of the new minimum value
             }
         }
-        changeElements(array, minIndex);
+        const temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
     }
     return array;
 }
 
 console.log(selectionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
+
+module.exports = selectionSort;
