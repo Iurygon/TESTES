8da66e0d6ba4d8d42e5fe88f531b7362513de224
Iury Gontijo Silva <iurygon.silva@gmail.com>
2023-12-02 22:55:17 -0300

    Add files via upload

---
+++ JavaScript/Sort/searching.js
@@ -0,0 +1,13 @@
+const arr = [-6, -2, 0,  2,  3,  4, 6,  7, 8, 13, 17, 20, 22, 97];
+
+function searching(array, from, to, element){
+//array it's the ordened array where the search will be executed, from and to are the inital e final elements where the search will be done, element it's the element searched 
+    const middle = Math.floor((from + to) / 2);
+    const actual = array[meio];
+    if (from > to){
+//to prevent erros, the first element must be minor then the last element where the search will be done
+        return -1
+    };
+    if ()
+
+};
